8a072be974be49fc6255a2682a3ee81a413bf73a
takagimeow <dev@takagi.app>
2021-09-08 12:02:27 +0900

    style: assigned each component a method

--- src/components/StickyHeader.tsx
+++ src/components/StickyHeader.tsx
@@ -1,24 +1,27 @@
 import React from 'react';
-import { Animated } from 'react-native';
+import { Animated, LayoutChangeEvent } from 'react-native';
 
 export interface IStickyHeader {
   children: React.ReactElement;
   style?: {
     height?: number | string;
   };
+  onLayout?: ((event: LayoutChangeEvent) => void) | undefined;
 }
 
-export function StickyHeader({ children, style }: IStickyHeader) {
+export function StickyHeader({ children, style, onLayout }: IStickyHeader) {
   return (
-    <Animated.View
-      style={{
-        ...style,
-        width: '100%',
-        alignItems: 'center',
-        justifyContent: 'center',
-      }}
-    >
-      {children}
+    <Animated.View onLayout={onLayout}>
+      <Animated.View
+        style={{
+          ...style,
+          width: '100%',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        {children}
+      </Animated.View>
     </Animated.View>
   );
 }
