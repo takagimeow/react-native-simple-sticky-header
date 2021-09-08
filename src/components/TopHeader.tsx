a4a0b0a75569fc83a15cb69cd8a24f871d34818b
takagimeow <dev@takagi.app>
2021-09-08 12:02:27 +0900

    style: assigned each component a method

--- src/components/TopHeader.tsx
+++ src/components/TopHeader.tsx
@@ -1,26 +1,29 @@
 import React from 'react';
-import { Animated } from 'react-native';
+import { Animated, LayoutChangeEvent } from 'react-native';
 
 export interface ITopHeader {
   children: React.ReactElement;
-  opacity: Animated.AnimatedInterpolation;
+  opacity?: number | Animated.Value | Animated.AnimatedInterpolation | undefined;
   style?: {
     height?: number | string;
   };
+  onLayout?: ((event: LayoutChangeEvent) => void) | undefined;
 }
 
-export function TopHeader({ children, opacity, style }: ITopHeader) {
+export function TopHeader({ children, opacity, style, onLayout }: ITopHeader) {
   return (
-    <Animated.View
-      style={{
-        ...style,
-        width: '100%',
-        alignItems: 'center',
-        justifyContent: 'center',
-        opacity,
-      }}
-    >
-      {children}
+    <Animated.View onLayout={onLayout}>
+      <Animated.View
+        style={{
+          ...style,
+          width: '100%',
+          alignItems: 'center',
+          justifyContent: 'center',
+          opacity,
+        }}
+      >
+        {children}
+      </Animated.View>
     </Animated.View>
   );
 }
